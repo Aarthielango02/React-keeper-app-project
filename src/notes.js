6fb843c49a534af4485439bf76bfb613ea8c4346
Aarthielango02 <163028754+Aarthielango02@users.noreply.github.com>
2024-06-22 00:05:48 +0530

    Add files via upload

---
+++ src/notes.js
@@ -0,0 +1,23 @@
+const notes=[
+    {
+        key:1,
+        title:"Delegation",
+        content:"Q. How many programmers does it take to change a light bulb?A.Its a hardware problem"
+    },
+    {
+        key:1,
+ title:"Loops",
+ content:"Q. How to keep a programmer in the shower forever.Show him the shampoo bottle intructions:Lather.Rinse.Repeat" 
+    },
+    {
+        key:1,
+        title:"Arrays",
+        content:"Why did the programmer quit his job?A. Because he didnt get arrays"
+    },
+    {
+        key:1,
+        title:"Hardware vs Software",
+        content:"Q.what is the difference between hardware and software? You can hit your hardware with ahammer,but you can only curde at your software"
+    }
+]
+export default notes
